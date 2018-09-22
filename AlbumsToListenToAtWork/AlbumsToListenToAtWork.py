b500d0ac37f0ac4487608fa5d181a169e8a4833c
hervster <hervenyemeck@gmail.com>
2018-09-22 05:44:46 -0400

    Added emailing python script and actual text file with album name.

--- AlbumsToListenToAtWork/AlbumsToListenToAtWork.py
+++ AlbumsToListenToAtWork/AlbumsToListenToAtWork.py
@@ -1,12 +1,31 @@
 # Author : Herve Nyemeck, Jessy Ma <3
+# Copy lines from google doc
 # Randomly pick a line from pre-existing file
 # Output said line and send to user
 
 #Album_List = open("Albums", "r") 
 # print (Album_List)
+import sendEmail 
 from random import randint
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from datetime import datetime
+import os
+
 albumListFile = "Test.txt"
 oldAlbumsListFile = "TestOld.txt"
+today = "today.txt"
+
+def removePreexisting():
+    exists = os.path.isfile(today)
+    if exists:
+        os.remove(today)
+    else:
+        return  
+
+removePreexisting()
 
 file = open(albumListFile,"r")
 lines = []
@@ -35,8 +54,13 @@ while album in old_albums_list:
 old_albums.close()
 old_albums = open(oldAlbumsListFile,"a")
 old_albums.write(album)
-print(album)
+# print(album)
 file.close()
+today_album = open(today, "w")
+today_album.write(album)
+today_album.close()
+sendEmail.sendEmail()
 #print (file.readlines())
 
+# msg = EmailMessage()
 #runItBack()
---
+++ AlbumsToListenToAtWork/sendEmail.py
@@ -0,0 +1,37 @@
+def sendEmail():
+
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from datetime import datetime
+
+    today = "today.txt"
+    dateNow = datetime.now()
+    msg = MIMEMultipart()
+    msg['From'] = 'herveselenium@gmail.com'
+    msg['To'] = 'hervenyemeck@gmail.com'
+    msg['Subject'] = 'Album to Listen to at Work for %02d/%02d/%04d ' % (dateNow.day, dateNow.month, dateNow.year)
+    dateNow = datetime.now()
+    monthNow = str(dateNow.day).encode('ascii')
+    message = 'Yo Hervé. Your album for %02d/%02d/%04d is:' % (dateNow.day, dateNow.month, dateNow.year)
+    msg.attach(MIMEText(message))
+    filename = today
+    file = open(today, "rb")
+    p= MIMEBase('application', 'octet-stream')
+    p.set_payload((file).read())
+    p.add_header('Content-Disposition', "file; filename= %s" % filename) 
+    msg.attach(p)
+
+    mailserver = smtplib.SMTP('smtp.gmail.com',587)
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    mailserver.login('herveselenium@gmail.com', 'Selen!umRul3s')
+
+    mailserver.sendmail('herveselenium@gmail.com','hervenyemeck@gmail.com',msg.as_string())
+
+    mailserver.quit()
